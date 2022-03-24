39a561b5b5f3a907594374727b3a87ed48a29703
akolpakov <akolpakov@aligntech.com>
2022-03-24 13:23:54 +0300

    feat: change cors settings

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(cors({
-    origin: 'https://olgin-sad.herokuapp.com/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs)
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
